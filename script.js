a8998ca44709ea0f1378e2ddef9876bc90c7dc53
xF1qshyyy <pokermoongoop@gmail.com>
2026-07-24 10:29:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+// ===============================
+// BEAST Tag Generator
+// Part 1
+// ===============================
 
+const canvas = document.getElementById("tagCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.imageSmoothingEnabled = false;
+
+const baseImage = new Image();
+
+baseImage.src = "beast-base.png";
+
+baseImage.onload = () => {
+
+    redraw();
+
+};
+
+function redraw(){
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.drawImage(
+
+        baseImage,
+
+        0,
+        0,
+
+        canvas.width,
+        canvas.height
+
+    );
+
+}
